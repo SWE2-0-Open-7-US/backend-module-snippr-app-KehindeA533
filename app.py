f7676328dad30b1b1418015a6fe9a8d0f5e93026
Kehinde. A <whenkehindewalks@gmail.com>
2023-10-06 14:20:20 -0400

    create queries added

--- app.py
+++ app.py
@@ -20,3 +20,26 @@ connection = psycopg2.connect(
 )
 
 # Define SQL queries
+# CREATE
+CREATE_USER = """CREATE TABLE IF NOT EXISTS user (
+    id SERIAL PRIMARY KEY,
+    email TEXT,
+    password TEXT
+);"""
+
+CREATE_SNIPPET = """cREATE TABLE IF NOT EXISTS snippet (
+    id SERIAL PRIMARY KEY,
+    language TEXT,
+    code TEXT,
+    snippet_id INT
+    FOREIGN KEY(snippEt_id) REFERENCES user(id) ON DELETE CASCADE
+);"""
+
+
+#
+# Endpoints
+# - Create a new snippet
+# - Get all the snippets
+# - Get a specific snippets
+
+# - Bonus: Users can make a GET request to e.g. /snippet?lang=python to retrieve all the Python snippets
